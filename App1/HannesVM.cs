379fe646d68b30213d603106a2885aedd8dee80d
Hannes Preishuber <hannesp@ppedv.de>
2017-10-05 15:41:47 +0200

    Easy Viewmodel

---
+++ App1/HannesVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+   public class HannesVM:INotifyPropertyChanged
+    {
+        public Person MyPerson { get; set; } = new Person();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Lade()
+        {
+               MyPerson.ID = 1;
+            MyPerson.Text = "Hannes";
+
+        }
+        
+
+    }
+}
---
+++ App1/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public int ID { get; set; }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
