786817baeb2f3500138aaa67454f9f28f8dde130
Hannes Preishuber <hannesp@ppedv.de>
2017-10-05 16:02:49 +0200

    Viewmodel Offline Liste

--- App1/HannesVM.cs
+++ App1/HannesVM.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace App1
 {
    public class HannesVM:INotifyPropertyChanged
     {
         public Person MyPerson { get; set; } = new Person();
+        public ObservableCollection<Customer> CustListe { get; set; } = new ObservableCollection<Customer>();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -20,7 +24,17 @@ namespace App1
             MyPerson.Text = "Hannes";
 
         }
-        
+        public async Task ladeKundenAsync()
+        {
+            var f = await KnownFolders.MusicLibrary.GetFileAsync("northwind.json");
+            var str=await FileIO.ReadTextAsync(f);
+            var items = JsonConvert.DeserializeObject<List<Customer>>(str);
+            foreach (var item in items)
+            {
+                CustListe.Add(item);
+            }
+        }
+
 
     }
 }
