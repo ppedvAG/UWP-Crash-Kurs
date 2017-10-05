741f6a939d5a21819f46154a752d246bf47153fc
Hannes Preishuber <hannesp@ppedv.de>
2017-10-05 10:43:56 +0200

    RSS Feed Listbox

---
+++ App1/RSSFeed.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace App1
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class RSSFeed : Page
+    {
+        public RSSFeed()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var client = new SyndicationClient();
+            var url = new Uri("https://www.heise.de/newsticker/heise-atom.xml");
+            var feed =await client.RetrieveFeedAsync(url);
+            liste1.ItemsSource = feed.Items;
+        }
+    }
+}
