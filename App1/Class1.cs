4fc636be93db7134b3a275388bc42bb5426c09e9
Hannes Preishuber <hannesp@ppedv.de>
2017-10-05 14:04:21 +0200

    Nortwind Listview

---
+++ App1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    
+    public class Rootobject
+    {
+        public List<Customer> Customers { get; set; }
+        public Responsestatus ResponseStatus { get; set; }
+    }
+
+    public class Responsestatus
+    {
+    }
+
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Region { get; set; }
+    }
+
+}
---
+++ App1/northwindliste.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace App1
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class northwindliste : Page
+    {
+        List<Customer> liste = new List<Customer>();
+        public northwindliste()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var result= await client.GetStringAsync("http://northwind.servicestack.net/customers?format=json");
+           
+
+            var ob = JsonConvert.DeserializeObject<Rootobject>(result);
+            liste = ob.Customers;
+
+            listview1.ItemsSource = liste;
+
+        }
+    }
+}
