78ec1eed4b5379892d62e6019b8d233e9efc68ec
Hannes Preishuber <hannesp@ppedv.de>
2017-10-05 13:45:32 +0200

    Storybaordsample

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -66,7 +66,7 @@ namespace App1
                     // Wenn der Navigationsstapel nicht wiederhergestellt wird, zur ersten Seite navigieren
                     // und die neue Seite konfigurieren, indem die erforderlichen Informationen als Navigationsparameter
                     // übergeben werden
-                    rootFrame.Navigate(typeof(animiert), e.Arguments);
+                    rootFrame.Navigate(typeof(automatischeAnimation), e.Arguments);
                 }
                 // Sicherstellen, dass das aktuelle Fenster aktiv ist
                 Window.Current.Activate();
